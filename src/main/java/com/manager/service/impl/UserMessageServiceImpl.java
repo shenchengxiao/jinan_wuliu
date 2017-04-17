8be3f29928bd2c65249794b4067e146852b5835b
ljs <15102616735@163.com>
2017-04-17 14:31:10 +0800

    极光推送给手机客户端发送消息通知 系统通知

--- src/main/java/com/manager/service/impl/UserMessageServiceImpl.java
+++ src/main/java/com/manager/service/impl/UserMessageServiceImpl.java
@@ -10,9 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.manager.exception.DatabaseException;
+import com.manager.mapper.UserMapper;
 import com.manager.mapper.UserMessageMapper;
+import com.manager.pojo.User;
 import com.manager.pojo.UserMessage;
 import com.manager.service.UserMessageService;
+import com.manager.utils.PushExample;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -20,34 +23,41 @@ import com.rabbitmq.client.ConnectionFactory;
 @Service
 public class UserMessageServiceImpl implements UserMessageService {
 	
-	 private static final String EXCHANGE_NAME = "JINAN_MESSAGE_EXCHANGE"; 
+//	 private static final String EXCHANGE_NAME = "JINAN_MESSAGE_EXCHANGE"; 
 	 
 	 @Resource
 	 private UserMessageMapper userMessageMapper;
+	 @Resource
+	 private UserMapper userMapper;
 	 Logger LOG = LoggerFactory.getLogger(BlackWordServiceImpl.class);
 	 
 	@Override
 	public boolean sendUserMessage(List<Integer> list, String content) throws DatabaseException {
 		try {
 			int val = -1;
+//			int andriod = 0;
+//			int ios = 0;
+//			String StrAndriod[]=new String[]{};
+//			String StrIOS[]=new String[]{};
+			
             if (content == null){
                 LOG.error("sendUserMessage 信息为空",content);
                 return false;
             }
             // 创建连接和频道  
-    		ConnectionFactory factory = new ConnectionFactory();  
-    		factory.setHost("localhost");
+//    		ConnectionFactory factory = new ConnectionFactory();  
+//    		factory.setHost("139.129.232.78");
             //端口
-            factory.setPort(5672);
+//            factory.setPort(5672);
 //            factory.setPort(5670);
             //设置账号信息，用户名、密码、vhost
-            factory.setUsername("guest");
-            factory.setPassword("guest");
-            factory.setVirtualHost("/");
-    		Connection connection = factory.newConnection();  
-    		Channel channel = connection.createChannel();  
+//            factory.setUsername("admin");
+//            factory.setPassword("adminjinan");
+//            factory.setVirtualHost("/jinan");
+//    		Connection connection = factory.newConnection();  
+//    		Channel channel = connection.createChannel();  
     		// 声明转发器的类型  
-    		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+//    		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
             for (Integer userid : list) {
             	UserMessage userMessage = new UserMessage();
 				userMessage.setUserId(userid);
@@ -58,13 +68,31 @@ public class UserMessageServiceImpl implements UserMessageService {
             	if(val > 0){
             		//MQ发送消息
             		// 发布消息至转发器，指定routingkey  
-                    channel.basicPublish(EXCHANGE_NAME, userid+"", null, content  
-                            .getBytes());  
-                    System.out.println(" [x] Sent '" + content + "'");
+//                    channel.basicPublish(EXCHANGE_NAME, userid+"", null, content  
+//                            .getBytes());  
+//                    System.out.println(" [x] Sent '" + content + "'");
+            		User user = userMapper.selectByPrimaryKey(userid);
+            		if(user != null){
+            			if(user.getPlatformType() != null && user.getPlatformType().getValue() == 2 && user.getRegistrationid() != null){
+            				PushExample.SendUsersPushToAndroid("济南网通知",content,user.getRegistrationid());
+//            				StrAndriod[andriod] = user.getRegistrationid();
+//            				andriod ++;
+            			}else if(user.getPlatformType() != null && user.getPlatformType().getValue() == 1 && user.getRegistrationid() != null){
+            				PushExample.SendUsersPushToIOS(content, user.getRegistrationid());
+//							StrIOS[ios] = user.getRegistrationid();
+//							ios ++;
+						}
+            		}
             	}
 			}
-            channel.close();  
-            connection.close();  
+//            if(StrAndriod.length > 0){
+//            	PushExample.SendUsersPushToAndroid("济南网通知",content,StrAndriod);
+//            }
+//            if(StrIOS.length > 0){
+//            	PushExample.SendUsersPushToIOS(content, StrIOS);
+//            }
+//            channel.close();  
+//            connection.close();  
             
            return (val > 0)?true:false;
         } catch (Throwable e) {
@@ -82,19 +110,19 @@ public class UserMessageServiceImpl implements UserMessageService {
                 return false;
             }
             // 创建连接和频道  
-    		ConnectionFactory factory = new ConnectionFactory();  
-    		factory.setHost("localhost");
+//    		ConnectionFactory factory = new ConnectionFactory();  
+//    		factory.setHost("localhost");
             //端口
-            factory.setPort(5672);
+//            factory.setPort(5672);
 //            factory.setPort(5670);
             //设置账号信息，用户名、密码、vhost
-            factory.setUsername("guest");
-            factory.setPassword("guest");
-            factory.setVirtualHost("/");
-    		Connection connection = factory.newConnection();  
-    		Channel channel = connection.createChannel();  
+//            factory.setUsername("guest");
+//            factory.setPassword("guest");
+//            factory.setVirtualHost("/");
+//    		Connection connection = factory.newConnection();  
+//    		Channel channel = connection.createChannel();  
     		// 声明转发器的类型  
-    		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+//    		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
             	UserMessage userMessage = new UserMessage();
 				userMessage.setContent(content);
 				userMessage.setCreateTime(new Date());
@@ -103,12 +131,13 @@ public class UserMessageServiceImpl implements UserMessageService {
             	if(val > 0){
             		//MQ发送消息
             		// 发布消息至转发器，指定routingkey  
-                    channel.basicPublish(EXCHANGE_NAME, "ALL", null, content  
-                            .getBytes());  
-                    System.out.println(" [x] Sent '" + content + "'");
+//                    channel.basicPublish(EXCHANGE_NAME, "ALL", null, content  
+//                            .getBytes());  
+//                    System.out.println(" [x] Sent '" + content + "'");
+            		PushExample.SendSysPush(content);
             	}
-            channel.close();  
-            connection.close();  
+//            channel.close();  
+//            connection.close();  
             
            return (val > 0)?true:false;
         } catch (Throwable e) {
---
+++ src/main/java/com/manager/utils/PushExample.java
@@ -0,0 +1,111 @@
+package com.manager.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.manager.pojo.JpushClint.JPushClient;
+
+import cn.jiguang.common.ClientConfig;
+import cn.jiguang.common.resp.APIConnectionException;
+import cn.jiguang.common.resp.APIRequestException;
+import cn.jpush.api.push.PushResult;
+
+public class PushExample {
+	
+	protected static final Logger LOG = LoggerFactory.getLogger(PushExample.class);
+	//在极光注册上传应用的 appKey 和 masterSecret  
+    private static final String APP_KEY ="3576d9a656173ef999d792c9";////必填，例如466f7032ac604e02fb7bda89  
+    private static final String MASTER_SECRET = "08e61e05a2df7e1edd01e374";//必填，每个应用都对应一个masterSecret  
+    public static long sendCount = 0;
+    
+	/**
+	 * 给所有人发送系统消息
+	 * @param content
+	 */
+	public static void SendSysPush(String content) {
+		
+		ClientConfig clientConfig = ClientConfig.getInstance();
+        final JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, clientConfig);
+        PushResult result = new PushResult();
+        
+        try {
+			result = jpushClient.sendNotificationAll(content);
+			LOG.info("Got result - " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+			e.printStackTrace();
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+		    LOG.info("HTTP Status: " + e.getStatus());
+		    LOG.info("Error Code: " + e.getErrorCode());
+		    LOG.info("Error Message: " + e.getErrorMessage());
+		    LOG.info("Msg ID: " + e.getMsgId());
+			e.printStackTrace();
+		}
+        System.out.println(result.getOriginalContent()+"====="+result.getRateLimitQuota()+
+				"========"+result.getRateLimitRemaining()+"========"+result.getRateLimitReset());
+        
+    }
+	/**
+	 * 发送信息给指定的安卓平台
+	 * @param title
+	 * @param msgContent
+	 * @param registrationID
+	 */
+	public static void SendUsersPushToAndroid(String title, String msgContent, String... registrationID) {
+		ClientConfig clientConfig = ClientConfig.getInstance();
+		Map<String, String> map = new HashMap<String, String>();
+        final JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, clientConfig);
+        PushResult result = new PushResult();
+        
+        try {
+			result = jpushClient.sendAndroidNotificationWithRegistrationID(title,msgContent,map,registrationID);
+			LOG.info("Got result - " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+			e.printStackTrace();
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+		    LOG.info("HTTP Status: " + e.getStatus());
+		    LOG.info("Error Code: " + e.getErrorCode());
+		    LOG.info("Error Message: " + e.getErrorMessage());
+		    LOG.info("Msg ID: " + e.getMsgId());
+			e.printStackTrace();
+		}
+        System.out.println(result.getOriginalContent()+"====="+result.getRateLimitQuota()+
+				"========"+result.getRateLimitRemaining()+"========"+result.getRateLimitReset());
+	}
+	/**
+	 * 发送信息给指定的ios用户
+	 * @param msgContent
+	 * @param registrationID
+	 */
+	public static void SendUsersPushToIOS(String msgContent, String... registrationID) {
+		ClientConfig clientConfig = ClientConfig.getInstance();
+		Map<String, String> map = new HashMap<String, String>();
+		final JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, clientConfig);
+		PushResult result = new PushResult();
+		
+		try {
+			result = jpushClient.sendIosNotificationWithRegistrationID(msgContent,map,registrationID);
+			LOG.info("Got result - " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+			e.printStackTrace();
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+			LOG.info("HTTP Status: " + e.getStatus());
+			LOG.info("Error Code: " + e.getErrorCode());
+			LOG.info("Error Message: " + e.getErrorMessage());
+			LOG.info("Msg ID: " + e.getMsgId());
+			e.printStackTrace();
+		}
+		System.out.println(result.getOriginalContent()+"====="+result.getRateLimitQuota()+
+				"========"+result.getRateLimitRemaining()+"========"+result.getRateLimitReset());
+	}
+    
+    
+}
